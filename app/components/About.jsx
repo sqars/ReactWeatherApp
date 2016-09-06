bf6af648a1d7351ff358f3722346ef93d4fdd44e
MaciejKawczynski <sqars89@gmail.com>
2016-09-06 10:13:41 +0200

    added custom styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var About = React.createClass({
   render: function(){
     return(
       <div>
-        <h1 className="text-center">About</h1>
+        <h1 className="text-center page-title">About</h1>
         <p>Hello, my name is Maciej and welcome to my WeatherApp!</p>
         <p>You can find code on gitHub: <a href="https://github.com/sqars/ReactWeatherApp">Git Repo</a></p>
 
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
       About: 'app/components/About.jsx',
       Examples: 'app/components/Examples.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
-      ErrorModal: 'app/components/errorModal.jsx'
+      ErrorModal: 'app/components/errorModal.jsx',
+      applicationStyles: 'app/styles/app.css'
     },
     extensions:['', '.js', '.jsx']
   },
